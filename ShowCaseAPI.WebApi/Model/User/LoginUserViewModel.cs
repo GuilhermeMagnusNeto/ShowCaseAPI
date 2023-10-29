c9ffdfef1e365d243a7d04ddae3535a019829e6d
Natalia Silva <natalia.nsdeveloper@gmail.com>
2023-10-29 20:35:21 -0300

    .

--- ShowCaseAPI.WebApi/Model/User/LoginUserViewModel.cs
+++ ShowCaseAPI.WebApi/Model/User/LoginUserViewModel.cs
@@ -4,7 +4,6 @@ namespace ShowCaseAPI.WebApi.Model.User
 {
     public class NewUserViewModel
     {
-        public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
