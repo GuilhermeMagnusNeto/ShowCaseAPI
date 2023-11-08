0cedd22fe73021e4588a3d5fc5671d4d02c80c94
Natalia Silva <natalia.nsdeveloper@gmail.com>
2023-11-08 20:20:24 -0300

    Link unico

--- ShowCaseAPI.WebApi/Model/Showcase/ShowcaseViewModel.cs
+++ ShowCaseAPI.WebApi/Model/Showcase/ShowcaseViewModel.cs
@@ -3,7 +3,7 @@
     public class ShowcaseViewModel
     {
         public Guid Id { get; set; }
-        public string ExclusiveCode { get; set; }
+        public string UniqueLink { get; set; }
         public string Name { get; set; }
         public Guid StoreId { get; set; }
     }
